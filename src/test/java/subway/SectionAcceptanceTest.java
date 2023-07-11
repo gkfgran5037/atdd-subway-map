d84f1016471ff8eb5753acde2b7f73caca42c422
gkfgran5037 <gkfgran5037@naver.com>
2023-07-11 23:04:46 +0900

    fix : 패키지 분리

--- src/test/java/subway/SectionAcceptanceTest.java
+++ src/test/java/subway/SectionAcceptanceTest.java
@@ -1,6 +1,7 @@
-package section;
+package subway;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @DisplayName("지하철 노선 관련 기능")
